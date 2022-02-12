6a6d1cf086124ee281e588f13423356e5e76c910
jun <qurugi0347@gmail.com>
2022-02-13 01:51:36 +0900

    Setting:배포세팅

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/numble-fe-01-color" element={<Home />} />
     </Routes>
   );
 };
